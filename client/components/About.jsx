e066145a4656bc251cc896731594698938c44067
littleprincey <orlandow1928@gmail.com>
2020-12-26 20:35:32 +1300

    remove sign-up feature

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -12,7 +12,7 @@ const Nav = (props) => {
           <div>
             <div>
               <p> 
-                I would never have thought that I would be happy to spend so much time on the internet, but the summer may as well not be happening in my world as I sit here joyously nerding out in the Cyber world. 
+                I would never have thought that I would be happy to spend so much time on the internet, but the summer may as well not be happening in my world as I sit here joyously nerding out in the cyber world. 
                 I'm lucky enough to be with my family on holiday. There is no internet up here so every day I walk an hour in the beautiful hot sun to the local library for study.
               </p>
             </div>
