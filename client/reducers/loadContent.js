6834845d6156450ab14242f6a9b90fbefda712cb
littleprincey <orlandow1928@gmail.com>
2021-01-01 15:33:25 +1300

    change setloaded reducer

--- client/reducers/loadContent.js
+++ client/reducers/loadContent.js
@@ -1,8 +1,10 @@
+import { SET_LOADED } from '../actions'
+
 const initialState = false
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_LOADED':
+    case SET_LOADED:
       return true
       default: return state
   }
