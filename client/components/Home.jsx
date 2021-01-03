eb8c0b423da0dcfab7cb7d53ac5da6a2f0b5d0b7
littleprincey <orlandow1928@gmail.com>
2021-01-03 17:23:17 +1300

    update latest on home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -31,9 +31,10 @@ const Home = (props) => {
           <div>
           <h3>Latest:</h3>
             <div> 
-              <p>{props.blogs[1].title}</p>
+              {/* <p>{props.blogs[1].title}</p>
               <p>{props.blogs[1].date}</p>
-              <p>{props.blogs[1].content}</p>
+              <p>{props.blogs[1].content}</p> */}
+              <p>alongside the udemy course, im learning about AWS, and cloud cyber security in general.</p>
             </div>
           </div>
           
